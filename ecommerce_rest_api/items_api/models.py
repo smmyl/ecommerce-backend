d3b1aab43b882e67808ae595870544906af4f1e2
Sammy <sammy.liaop@gmail.com>
2023-04-18 00:06:43 -0400

    about page done

--- ecommerce_rest_api/items_api/models.py
+++ ecommerce_rest_api/items_api/models.py
@@ -4,32 +4,32 @@ from django.contrib.postgres.fields import ArrayField
 # Create your models here.
 class Items(models.Model):
     ITEM_CATEGORY = [
-        ('C','Clothing'),
-        ('S','Shoes'),
-        ('A','Acessories')
+        ('Clothing','Clothing'),
+        ('Shoes','Shoes'),
+        ('Acessories','Acessories')
     ]
     GENDER = [
-        ('M', 'Male'),
-        ('F', 'Female'),
+        ('M', 'M'),
+        ('F', 'F'),
         ('N', 'Neutral')
     ]
     CLOTHING_SIZE = [
-        ('XS', 'X-Small'),
-        ('S', 'Small'),
-        ('M', 'Medium'),
-        ('L', 'Large'),
-        ('XL', 'X-Large'),
-        ('XXL', 'XX-Large')
+        ('XS', 'XS'),
+        ('S', 'S'),
+        ('M', 'M'),
+        ('L', 'L'),
+        ('XL', 'XL'),
+        ('XXL', 'XXL')
     ]
     SHOE_SIZE = [
-        (0, 0), (0.5, 0.5), (1, 1), (1.5, 1.5), (2, 2), (2.5, 2.5), (3, 3), (3.5, 3.5), (4, 4), (4.5, 4.5), (5, 5), (5.5, 5.5), (6, 6), (6.5, 6.5), (7, 7), (7.5, 7.5), (8, 8), (8.5, 8.5), (9, 9), (9.5, 9.5), (10, 10), (10.5, 10.5), (11, 11), (11.5, 11.5), (12, 12), (12.5, 12.5), (13, 13), (13.5, 13.5), (14, 14), (14.5, 14.5), (15, 15), (15.5, 15.5), (16, 16), (16.5, 16.5), (17, 17)
+        ('0', '0'), ('0.5', '0.5'), ('1', '1'), ('1.5', '1.5'), ('2', '2'), ('2.5', '2.5'), ('3', '3'), ('3.5', '3.5'), ('4', '4'), ('4.5', '4.5'), ('5', '5'), ('5.5', '5.5'), ('6', '6'), ('6.5', '6.5'), ('7', '7'), ('7.5', '7.5'), ('8', '8'), ('8.5', '8.5'), ('9', '9'), ('9.5', '9.5'), ('10', '10'), ('10.5', '10.5'), ('11', '11'), ('11.5', '11.5'), ('12', '12'), ('12.5', '12.5'), ('13', '13'), ('13.5', '13.5'), ('14', '14'), ('14.5', '14.5'), ('15', '15'), ('15.5', '15.5'), ('16', '16'), ('16.5', '16.5'), ('17', '17')
     ]
     item_category = models.CharField(choices=ITEM_CATEGORY)
     image = models.CharField()
     gender = models.CharField(choices=GENDER)
-    name = models.CharField(max_length=20)
+    name = models.CharField()
     price = models.IntegerField()
-    tags = ArrayField(models.CharField(max_length=20))
-    description = models.CharField(max_length=200)
+    tags = ArrayField(models.CharField())
+    description = models.CharField()
     clothing_size = models.CharField(choices=CLOTHING_SIZE, blank=True)
-    shoe_size = models.DecimalField(max_digits=5, decimal_places=1, choices=SHOE_SIZE)
+    shoe_size = models.CharField()
